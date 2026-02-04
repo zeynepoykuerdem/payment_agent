8a802e33b63c1eb7f4a41d7481c7c243c0792bd5
zeynepoykuerdem <zeynepoykuerdem@gmail.com>
2026-02-04 21:07:48 +0100

    adjust ai model name

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ sheet_rundfunkt= client_googlesheets.open("rundfunkt_payments").sheet1
 sheet_vodafone= client_googlesheets.open("vodafone_payments").sheet1
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-client_ai= genai.GenerativeModel('gemini-1.5-flash')
+client_ai= genai.GenerativeModel('models/gemini-1.5-flash')
 
 def talk_to_agent(chat_input):
     system_prompt= f""" You are a helpful payment management assistant. 
